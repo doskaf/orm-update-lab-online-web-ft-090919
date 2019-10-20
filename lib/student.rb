430c44eabc8aac149f1c51a63fd56f46ffa0ee14
Dominique Skaf <doskaf@yahoo.com>
2019-10-20 22:09:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,12 +47,19 @@ class Student
   end
   
   def self.create(name:, grade:)
-    student = Student.new(name, grade)
+    student = self.new(name, grade)
     student.save
     student
   end
   
-  def self.new_from_db
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end
+  
+  def self.find_by_name(name)
     
   end
   
